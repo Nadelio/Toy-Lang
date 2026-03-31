e2ded62188c67b75b3d91bb97cd61f0b7d4f0640
Nadelio <carefulsniffle@gmail.com>
2026-03-31 00:59:09 -0500

    Flag processing

--- main.c
+++ main.c
@@ -2,15 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
 #include <string_slice.h>
-
-/// @brief Compares if to strings are equivalent
-/// @param str_1 
-/// @param str_2
-bool streql(const char* str_1, const char* str_2) {
-    return strcmp(str_1, str_2) == 0;
-}
 
 /*
 OP CODES:
@@ -107,6 +99,8 @@ Command line arguments:
 
 int main(int argc, char** argv) {
 
+    const char* help_message = "--help              | -h            : print the help message\n--verbose           | -v            : verbose information about parser and runtime states\n--export            | -x            : export the binary for the program\n--input <filename>  | -f <filename> : use the following file as a program\n--output <filename> | -o <filename> : define the name for the exported program (defaults to program.bin)\n";
+
     bool verbose = false;
 
     bool use_existing_file = false;
@@ -115,9 +109,64 @@ int main(int argc, char** argv) {
     bool export_binary = false;
     char* name_of_output_file;
 
+    // convert all the static strings to slices
+    StringSlice verbose_slice = string_to_slice("--verbose");
+    StringSlice v_slice = string_to_slice("-v");
+    StringSlice help_slice = string_to_slice("--help");
+    StringSlice h_slice = string_to_slice("-h");
+    StringSlice export_slice = string_to_slice("--export");
+    StringSlice x_slice = string_to_slice("-x");
+    StringSlice input_slice = string_to_slice("--input");
+    StringSlice f_slice = string_to_slice("-f");
+    StringSlice output_slice = string_to_slice("--output");
+    StringSlice o_slice = string_to_slice("-o");
+
+    // process command line arguments
     for(int i = 1; i < argc; i++) {
+        // convert args to slices
+        StringSlice curr_arg = string_to_slice(argv[i]);
         
+
+        if(streql(&curr_arg, &verbose_slice) || streql(&curr_arg, &v_slice)) {
+            verbose = true;
+        }
+        if(streql(&curr_arg, &help_slice) || streql(&curr_arg, &h_slice)) {
+            printf("%s", help_message);
+        }
+        if(streql(&curr_arg, &export_slice) || streql(&curr_arg, &x_slice)) {
+            export_binary = true;
+        }
+        if(streql(&curr_arg, &input_slice) || streql(&curr_arg, &f_slice)) {
+            StringSlice next_arg;
+            if(argc > i + 1) { next_arg = string_to_slice(argv[i + 1]); }
+            else {
+                printf("Missing a file name for input flag.");
+                return 1;
+            }
+
+            use_existing_file = true;
+            name_of_input_file = slice_to_string(next_arg);
+        }
+        if(streql(&curr_arg, &output_slice) || streql(&curr_arg, &o_slice)) {
+            StringSlice next_arg;
+            if(argc > i + 1) { next_arg = string_to_slice(argv[i + 1]); }
+            else {
+                printf("Missing a file name for output flag.");
+                return 1;
+            }
+
+            export_binary = true;
+            name_of_output_file = slice_to_string(next_arg);
+        }
     }
+
+    if(argc == 1) {
+        printf("No flags passed in.");
+        return 0;
+    }
+
+    // debug for flags
+    printf("Flags:\n  Verbose? %d\n  Input file? %d Name: %s\n  Export binary? %d Name: %s\n", verbose, use_existing_file, name_of_input_file, export_binary, name_of_output_file);
 
     return 0;
 }
